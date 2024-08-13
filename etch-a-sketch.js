cad9b5c7d49a32a90ec9b8e0d4f349921f03d3a8
ShinpeiH <dctechsf@gmail.com>
2024-08-13 14:10:21 -0700

    create js file and main container div

---
+++ etch-a-sketch.js
@@ -0,0 +1,12 @@
+// console.log('hello'); // works
+const sqDiv = document.createElement("div");
+
+// container.appendChild(sqDiv); // this shows up as a dark line up top
+// sqDiv.style.border = "5px solid black";
+// container.appendChild(sqDiv);
+// create a function that takes a parameter and creates a 
+// parameter squared number of square divs inside the container
+
+function createGrid(side) {
+
+}
