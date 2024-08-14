1f79369ff231b4208f6a5bea119518721ee16c17
ShinpeiH <dctechsf@gmail.com>
2024-08-14 13:01:39 -0700

    create main container and a function that creates equal sized columns

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,12 +1,19 @@
-// console.log('hello'); // works
-const sqDiv = document.createElement("div");
-
-// container.appendChild(sqDiv); // this shows up as a dark line up top
-// sqDiv.style.border = "5px solid black";
-// container.appendChild(sqDiv);
 // create a function that takes a parameter and creates a 
 // parameter squared number of square divs inside the container
 
-function createGrid(side) {
-
+// this function below will create divs side by side
+// horizontally
+function createColumn(num) {
+    for (let i = 1; i <= num; i++) {
+        const sqDiv = document.createElement("div");
+        const width = 100 / Number(num) // this is correct width percentage
+        sqDiv.style.border = "3px solid black";
+        sqDiv.style.height = "";
+        
+        console.log(`${width}`);
+        sqDiv.style.width = `${width}%`; // this creats even columns
+        container.appendChild(sqDiv);
+    }
 }
+
+createColumn(7)
