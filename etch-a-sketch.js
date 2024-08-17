dcede5734d883c7a5a155ebb997e5ef572d39afc
ShinpeiH <dctechsf@gmail.com>
2024-08-17 01:00:00 -0700

    cleaned up code, remove unneccessary code and comments

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,15 @@
-startBtn = document.createElement("button");
+const startBtn = document.createElement("button");
 startBtn.textContent = "Start";
-container = document.querySelector("#container");
+
+const container = document.querySelector("#container");
 document.body.insertBefore(startBtn, container);
 
 startBtn.addEventListener("click", () => {
     let num = prompt("Enter the number of squares per side: (must be less than 100)");
+
     container.innerHTML = '';
     createGrid(num);
 });
-
-//createGrid(50);
 
 function createGrid(num) {
     // this creates the columns
@@ -17,16 +17,13 @@ function createGrid(num) {
         const column = document.createElement("div");
         const width = 100 / Number(num) // this is correct width percentage   
         
-        column.style.width = `${width}%`; // this creats even columns
-        column.style.maxHeight = '100%'; // max height does work but won't prevent squares from going outside border
-        // column.style.boxSizing = "border-box"; // not sure if this is needed
+        column.style.width = `${width}%`; // this creates even columns
         container.appendChild(column);
 
         // this creates the squares inside the columns
         for (let i = 1; i <= num; i++) {
             const square = document.createElement("div");
-            // const sqHeight = width
-            square.style.border = "1px solid black"; // this causes the height to exceed the parent div
+            square.style.border = "1px solid black"; 
             square.style.boxSizing = "border-box"; // this prevents the height to exceedt the parent div
             square.style.height = `${width}%`;
             square.style.backgroundColor = 'yellow';
@@ -36,10 +33,10 @@ function createGrid(num) {
             square.addEventListener("mouseenter", () => { 
                 square.style.backgroundColor = "red";
             }); // mouseenter and mousemove works to hover but not click
-        }       // dragover, 
+        }     
     }
 }
 
-// createGrid(100)
+
 
 
